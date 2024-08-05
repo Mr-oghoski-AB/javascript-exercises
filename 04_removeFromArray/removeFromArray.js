45ebbbb27f830385b0df0b7e4e1f7bebba570e8f
oghovese imetowa <itzoghuski@gmail.com>
2024-08-05 23:28:39 +0100

    finished the first 5 exercise on odin js exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr1, ...num) {
+   let newArray = arr1.filter((element, index) => {
+        if ( !num.includes(element)){
+            return element;
+        }
+       
+    });
+    return newArray
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = null;
+if (num1 < 0 
+    || num2 < 0 
+    || typeof(num1) !== 'number' 
+    || typeof(num2) !== 'number'){
+    return "ERROR"
+}
+    if (num1 > num2) [num1, num2] = [num2, num1];
+    for (; num1 <= num2; num1++) {
+        finalSum += num1
+        
+    }
+    return finalSum
 };
 
 // Do not edit below this line
